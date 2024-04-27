af95c3dc80c4f2933c5aa053322bd5164956c0c9
MarvelStacks Dev1 <119876237+MarvelStacksDev1@users.noreply.github.com>
2024-04-27 10:05:19 +0530

    final

--- QRGenerator/index.js
+++ QRGenerator/index.js
@@ -3,6 +3,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const url = document.querySelector(".form-control");
   const btn = document.querySelector(".btn");
   const qrImg = document.querySelector(".qrImg");
+  const spinner = document.querySelector(".spinner-border");
 
   btn.addEventListener("click", function () {
     if (url.value == "") {
@@ -11,6 +12,10 @@ document.addEventListener("DOMContentLoaded", () => {
         url.classList.remove("error");
       }, 500);
     } else {
+      spinner.style.display = "block";
+      setTimeout(() => {
+        spinner.style.display = "none";
+      }, 100);
       qrImg.src = api + url.value;
       qrImg.classList.add("showImg");
     }
